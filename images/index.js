7d481bb5a51e0fa6fd58ba2f585932cf8dc76bc5
Yasmine03 <diagoneg1@student.iugb.edu.ci>
2024-10-15 17:31:59 +0000

    first commit

---
+++ images/index.js
@@ -0,0 +1,26 @@
+let currentIndex = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.carousel-item');
+    const totalSlides = slides.length;
+    if (index >= totalSlides) {
+        currentIndex = 0;
+    } else if (index < 0) {
+        currentIndex = totalSlides - 1;
+    } else {
+        currentIndex = index;
+    }
+    const newTransform = `translateX(-${currentIndex * 100}%)`;
+    document.querySelector('.carousel-inner').style.transform = newTransform;
+}
+
+function nextSlide() {
+    showSlide(currentIndex + 1);
+}
+
+function prevSlide() {
+    showSlide(currentIndex - 1);
+}
+
+// Initialisation
+showSlide(currentIndex);
